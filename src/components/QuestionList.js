79b599fce0dfe70da01a3ff68ba649f929a574b6
Jayvon Rhines <jayvonrhines0@gmail.com>
2023-09-19 12:23:30 -0500

    3 tests passing

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,37 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import QuestionItem from "./QuestionItem";
+
+
 
 function QuestionList() {
+  const [questionToDisplay, setQuestionToDisplay] = useState([])
+
+  function handleDelete(deletedQuestion) {
+    const updatedQuestion = questionToDisplay.filter((question) => question.id !== deletedQuestion.id)
+    setQuestionToDisplay(updatedQuestion)
+  }
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+    .then((r) => r.json())
+    .then((questionArray) => {
+      setQuestionToDisplay(questionArray)
+    })
+  }, [])
+
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {questionToDisplay.map((question) => ( 
+          <QuestionItem 
+           key={question.id} 
+           question={question} 
+           onDelete={handleDelete}/>
+        ))}
+
+      </ul>
     </section>
   );
 }
